d86f1be3a16e6f406a638ce8eb9cb5b12039c5b7
ДыоБранда <kirilleven@mail.ru>
2022-09-16 18:30:14 +0300

    something

--- src/Stackdev/FrontMiddleBack.java
+++ src/Stackdev/FrontMiddleBack.java
@@ -1,4 +1,15 @@
 package Stackdev;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class FrontMiddleBack {
+    Queue<Integer> queue;
+    FrontMiddleBack(){
+        queue = new LinkedList<>();
+    }
+
+    public void pushFront(int val){
+        queue.
+    }
 }
