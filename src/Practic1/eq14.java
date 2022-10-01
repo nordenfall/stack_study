754110992afdff283c724c9698792f8b7ab8e1d7
ДыоБранда <kirilleven@mail.ru>
2022-10-01 13:34:41 +0300

    isn't working.......

---
+++ src/Practic1/eq14.java
@@ -0,0 +1,42 @@
+package Practic1;
+
+
+import java.util.Scanner;
+
+public class eq14 {
+    public static void res(){
+        int sum = 0;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Количество чисел");
+        int i = scan.nextInt();
+        int[] mas = new int[i];
+        int min = 100000;
+        int max = 0;
+
+        for (int l = 0; l < i; l++){
+            int num = scan.nextInt();
+            if (scan.hasNextInt()){
+                mas[l] = num;
+                if(mas[l] > max){
+                    max = mas[l];
+                }
+                if(mas[l] < min){
+                    min = mas[l];
+                }
+            }
+        }
+
+        int id = 0;
+        while(id != i - 1){
+            sum += mas[id];
+            id++;
+        }
+        System.out.println("Сумма " + sum);
+        System.out.println("min" + min);
+        System.out.println("max" + max);
+    }
+
+    public static void main(String[] args){
+        res();
+    }
+}
